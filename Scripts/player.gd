ff67cad1aa2cd618ac026d0a32aa7a62f7c225fb
Dukatti <evertl@rocketmail.com>
2025-01-29 20:17:49 +0100

    added acceleration and friction to player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-
+const ACCELERATION = 600
+const FRICTION = 800
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 
@@ -11,15 +12,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	var direction := Input.get_axis("Left", "Right")
 	if direction != 0:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x,SPEED*direction, ACCELERATION*delta)	
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION*delta)
 
 	move_and_slide()
