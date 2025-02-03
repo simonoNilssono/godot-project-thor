0f7782116526c6c055fb79e555364635c563342c
Dukatti <evertl@rocketmail.com>
2025-02-03 12:12:47 +0100

    comments, added movement towards player on enemy

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,10 +1,18 @@
 extends CharacterBody2D
 
 const DEATH_SCENE = preload("res://Scenes/death_anim.tscn")
-const SPEED = 300.0
+const SPEED = 10.0
 const JUMP_VELOCITY = -400.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+var player : Node2D
 
+var target_velocity =  Vector2.ZERO
+var direction = Vector2.ZERO
+func _ready():
+	player = get_parent().get_child(1).get_child(0)
+	
+	
+	
 func _process(delta):
 	updateAnimations()
 	
@@ -12,15 +20,34 @@ func deathProcess():
 	var death_scene = DEATH_SCENE.instantiate()
 	death_scene.position = global_position
 	get_tree().root.add_child(death_scene)
-	print(get_tree().root)
 	
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+	addGravity(delta)
+	move2wardsPlayer(delta)
 	move_and_slide()
 
+# Enemy moves toward player along the floor
+func move2wardsPlayer(delta):
+	if is_on_floor():	
+		#set direction towards the player
+		direction.x = player.position.x - global_position.x
+		
+		#if needed flip sprite
+		if direction.x < 0:
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.flip_h = false	
+		
+		#move	
+		target_velocity.x = direction.x * SPEED
+		velocity =  target_velocity* delta
+
+#add gravity	
+func addGravity(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+		
 func updateAnimations():
 	animated_sprite_2d.play("run")	
